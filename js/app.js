1750d8bcb1737408625fb7a291c3137ea0a50dec
Steve Gossip <stephengossip@gmail.com>
2019-06-23 22:19:59 +0100

    refactored code to create a ul instead of just paragraphs containing values

--- js/app.js
+++ js/app.js
@@ -9,17 +9,44 @@ document.addEventListener('DOMContentLoaded', () => {
   deleteButton.addEventListener('click', handleDeleteButton)
 });
 
+// create Tekken list
+
+const createTekkenListCharacter = function (form) {
+  const tekkenListCharacter = document.createElement('li');
+  tekkenListCharacter.classList.add('tekken-list-character')
+
+  const fullName = document.createElement('h2');
+  fullName.textContent = `Name: ${form.full_name.value}`;
+  tekkenListCharacter.appendChild(fullName);
+
+  const fightingStyle = document.createElement('h3');
+  fightingStyle.textContent = `Fighting Style: ${form.fighting_style.value}`;
+  tekkenListCharacter.appendChild(fightingStyle);
+
+  const age = document.createElement('p');
+  age.textContent = `Age: ${form.age.value}`;
+  tekkenListCharacter.appendChild(age);
+
+  const continent = document.createElement('p');
+  continent.textContent = `Continent: ${form.continent.value}`;
+  tekkenListCharacter.appendChild(continent);
+
+  return tekkenListCharacter;
+}
+
 // form submit
 
 const handleFormSubmit = function (evt) {
   evt.preventDefault();
-  const newItem = document.createElement('li');
-  newItem.textContent = `Name: ${evt.target.full_name.value},  Age: ${evt.target.age.value},  Fighting Style: ${evt.target.fighting_style.value},  Continent:  ${evt.target.continent.value}`
+  // const newItem = document.createElement('li');
+  // newItem.textContent = `Name: ${evt.target.full_name.value},  Age: ${evt.target.age.value},  Fighting Style: ${evt.target.fighting_style.value},  Continent:  ${evt.target.continent.value}`
+  //
+  // const list = document.querySelector('ul');
+  // list.appendChild(newItem);
 
-  const list = document.querySelector('ul');
-  list.appendChild(newItem);
-
-
+  const tekkenListCharacter = createTekkenListCharacter(event.target);
+  const tekkenList = document.querySelector('ul');
+  tekkenList.appendChild(tekkenListCharacter);
 
   const resetFormSubmit = document.querySelector('#new-character-form')
   resetFormSubmit.reset();
@@ -33,35 +60,3 @@ const handleDeleteButton = function () {
   tekkenCharacterList.removeChild(tekkenCharacterList.firstChild);
   }
 };
-
-
-// const createTekkenListCharacter = function (form) {
-//   const tekkenListCharacter = document.createElement('li');
-//   tekkenListCharacter.classList.add('tekken-list-character')
-//
-//   const firstName = document.createElement('h2');
-//   firstName.textContent = form.first_name.value;
-//   tekkenListCharacter.appendChild(firstName);
-//
-//   const lastName = document.createElement('h2');
-//   lastName.textContent = form.last_name.value;
-//   tekkenListCharacter.appendChild(lastName);
-//
-//   const fightingStyle = document.createElement('h3');
-//   fightingStyle.textContent = form.fighting_style.value;
-//   tekkenListCharacter.appendChild(fightingStyle);
-//
-//   const age = document.createElement('p');
-//   age.textContent = form.age.value;
-//   tekkenListCharacter.appendChild(age);
-//
-//   const continent = document.createElement('p');
-//   continent.textContent = form.continent.value;
-//   tekkenListCharacter.appendChild(continent);
-//
-//   return tekkenListCharacter;
-// }
-//
-//   const tekkenListCharacter = createTekkenListCharacter(event.target);
-//   const tekkenList = document.querySelector('ul');
-//   tekkenList.appendChild(tekkenListCharacter);
